c3c072bfaa204aa72678b4ab8aa86902c7978e10
rigir <rafal.paszkowski.kontakt@gmail.com>
2023-05-29 15:17:22 +0200

    Added site responsiveness

--- services/frontend/src/pages/TransportForm/index.tsx
+++ services/frontend/src/pages/TransportForm/index.tsx
@@ -99,76 +99,84 @@ export default function index() {
                                                     </button>
                                                 </div>
                                             </div>
-                                            {form.errors.cargos && typeof form.errors.cargos === 'string' && (
-                                                <div className="form-field-error-message">{form.errors.cargos}</div>
-                                            )}
                                         </div>
-                                        <div className="form-cargo-list-items" contentEditable="false" data-ph="Add item to the list..">
-                                            {values.cargos.map((_, index) => {
-                                                const cargoName = `cargos[${index}].name`;
-                                                const cargoWeight = `cargos[${index}].weight_in_kg`;
-                                                const cargoType = `cargos[${index}].type`;
+                                        <div>
+                                            {!values.cargos.length ?
+                                                (<div className="form-cargo-list-items">{
+                                                    form.errors.cargos && typeof form.errors.cargos === 'string' ? (
+                                                        <div className="form-cargo-list-item-placeholder error">{form.errors.cargos}</div>
+                                                    ) : (<div className="form-cargo-list-item-placeholder" >List of items...</div>)
+                                                }
+                                                </div>) :
+                                                (<div className="form-cargo-list-items">{
+                                                    values.cargos.map((_, index: number) => {
+                                                        const cargoName = `cargos[${index}].name`;
+                                                        const cargoWeight = `cargos[${index}].weight_in_kg`;
+                                                        const cargoType = `cargos[${index}].type`;
 
-                                                return (
-                                                    <div key={index} className="form-cargo-list-item">
-                                                        <div>
-                                                            <div className="form-field-container">
-                                                                <label htmlFor={cargoName}>Name:</label>
-                                                                <Field name={cargoName} type="text" />
+                                                        return (
+                                                            <div key={index} className="form-cargo-list-item">
+                                                                <div>
+                                                                    <div className="form-field-container">
+                                                                        <label htmlFor={cargoName}>Name:</label>
+                                                                        <Field name={cargoName} type="text" />
+                                                                    </div>
+                                                                    <div className="form-field-error-message">
+                                                                        <ErrorMessage name={cargoName} />
+                                                                    </div>
+                                                                </div>
+                                                                <div>
+                                                                    <div className="form-field-container">
+                                                                        <label htmlFor={cargoWeight}>Weight:</label>
+                                                                        <Field name={cargoWeight} type="number" />
 
+                                                                    </div>
+                                                                    <div className="form-field-error-message">
+                                                                        <ErrorMessage name={cargoWeight} />
+                                                                    </div>
+                                                                </div>
+                                                                <div>
+                                                                    <div className="form-field-container">
+                                                                        <label htmlFor={cargoType}>Type:</label>
+                                                                        <Field as="select" id="type" name={cargoType}>
+                                                                            <option value="" disabled>Select an cargo type</option>
+                                                                            {transportForm.cargo_types.map((type: string, index: number) => (
+                                                                                <option key={index} value={type}>
+                                                                                    {type}
+                                                                                </option>
+                                                                            ))}
+                                                                        </Field>
+                                                                    </div>
+                                                                    <div className="form-field-error-message">
+                                                                        <ErrorMessage name={cargoType} />
+                                                                    </div>
+                                                                </div>
+                                                                <div>
+                                                                    <div className="form-field-container">
+                                                                        <button type="button" onClick={() => remove(index)}>
+                                                                            Remove
+                                                                        </button>
+                                                                    </div>
+                                                                </div>
                                                             </div>
-                                                            <div className="form-field-error-message">
-                                                                <ErrorMessage name={cargoName} />
-                                                            </div>
-                                                        </div>
-                                                        <div>
-                                                            <div className="form-field-container">
-                                                                <label htmlFor={cargoWeight}>Weight:</label>
-                                                                <Field name={cargoWeight} type="number" />
-
-                                                            </div>
-                                                            <div className="form-field-error-message">
-                                                                <ErrorMessage name={cargoWeight} />
-                                                            </div>
-                                                        </div>
-                                                        <div>
-                                                            <div className="form-field-container">
-                                                                <label htmlFor={cargoType}>Type:</label>
-                                                                <Field as="select" id="type" name={cargoType}>
-                                                                    <option value="" disabled>Select an cargo type</option>
-                                                                    {transportForm.cargo_types.map((type: string, index: number) => (
-                                                                        <option key={index} value={type}>
-                                                                            {type}
-                                                                        </option>
-                                                                    ))}
-                                                                </Field>
-                                                            </div>
-                                                            <div className="form-field-error-message">
-                                                                <ErrorMessage name={cargoType} />
-                                                            </div>
-                                                        </div>
-                                                        <div>
-                                                            <div className="form-field-container">
-                                                                <button type="button" onClick={() => remove(index)}>
-                                                                    Remove
-                                                                </button>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                );
-                                            })}
+                                                        );
+                                                    }
+                                                    )
+                                                }
+                                                </div>
+                                                )
+                                            }
                                         </div>
-                                    </div>
-                                )
-                                }
+                                    </div>)}
                             </FieldArray>
                         </div>
                         <div className="form-submit-button">
                             <button type="submit">Submit</button>
                         </div>
-                    </Form>
-                )}
-            </Formik>
+                    </Form >
+                )
+                }
+            </Formik >
         </div >
     );
 }
